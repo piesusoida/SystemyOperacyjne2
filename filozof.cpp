b20b124b132a266de30796e9f8da0cb6a318cf76
piesusoida <134595647+piesusoida@users.noreply.github.com>
2025-04-02 18:25:01 +0200

    Update filozof.cpp
    translating the comments to english

--- filozof.cpp
+++ filozof.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 struct Fork{
-    pair <int, int> fork_position; // gdzie jest na stole
+    pair <int, int> fork_position; // position on the table
     mutex used;
 };
 
@@ -19,17 +19,14 @@ void philosopher(int id, vector <Fork>& forks) {
         cout << "Philosopher " << id << " is thinking" << endl;
         this_thread::sleep_for(chrono::milliseconds(thinking_time));  
 
-        // Pick up left fork
-        unique_lock<mutex> lockLeft(forks[forks[id].fork_position.first].used);
+        unique_lock<mutex> lockLeft(forks[forks[id].fork_position.first].used); //picking up left fork
 
-        // Pick up right fork
-        unique_lock<mutex> lockRight(forks[forks[id].fork_position.second].used);
+        unique_lock<mutex> lockRight(forks[forks[id].fork_position.second].used); // picking up right fork
 
         cout << "Philosopher " << id << " is eating" << endl;
-        this_thread::sleep_for(chrono::milliseconds(1000));  // Eating
+        this_thread::sleep_for(chrono::milliseconds(1000)); 
 
-        // Release forks
-        lockLeft.unlock();
+        lockLeft.unlock(); // giving back the forks
         lockRight.unlock();
 
         cout << "Philosopher " << id << " is full, he starts wasting time thinking" << endl;;
@@ -53,12 +50,12 @@ int main(){
     }
     vector < Fork > forks(number_of_philosophers);
     vector < thread > philosophers;
-    for(int i=0;i<number_of_philosophers;i++){       
+    for(int i=0;i<number_of_philosophers;i++){        //creating threads, full lock is prevented by random time of thinking and numeration of forks
         forks[i].fork_position.first=i;
         forks[i].fork_position.second=(i+1)%number_of_philosophers;
         philosophers.push_back(thread(philosopher, i, ref(forks)));
     }
-    for (auto& philosopher : philosophers) {
+    for (auto& philosopher : philosophers) { //synchronization
         philosopher.join();
     }
 
